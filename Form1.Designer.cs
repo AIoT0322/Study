ada7af8f44172e2bfd03b19ab393e3031b9d2982
AIoT0322 <129762568+AIoT0322@users.noreply.github.com>
2023-09-10 15:25:53 +0900

    숫자 맞추기 게임
    C# 윈폼 숫자 맞추기 게임 코드

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Numberfind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "게임을 시작합니다!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.input.Location = new System.Drawing.Point(270, 94);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(75, 23);
+            this.input.TabIndex = 1;
+            this.input.Text = "입력";
+            this.input.UseVisualStyleBackColor = true;
+            this.input.Click += new System.EventHandler(this.inputclicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(107, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // start
+            // 
+            this.start.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.start.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.start.Location = new System.Drawing.Point(0, 167);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(449, 32);
+            this.start.TabIndex = 3;
+            this.start.Text = "게임 시작";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.startclicked);
+            // 
+            // display
+            // 
+            this.display.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.display.Location = new System.Drawing.Point(103, 41);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(266, 19);
+            this.display.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(449, 199);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button input;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label display;
+    }
+}
+
