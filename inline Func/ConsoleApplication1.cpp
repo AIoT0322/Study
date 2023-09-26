ccb0dda140dd6a691553892940dba9a2bc4ada75
AIoT0322 <129762568+AIoT0322@users.noreply.github.com>
2023-09-26 09:33:31 +0900

    inline함수 예제

---
+++ inline Func/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//인라인함수 예제
+
+#include <iostream>
+using namespace std;
+
+/*inline int SQUARE(int x)
+{
+    return x * x;
+}
+
+int main()
+{
+    cout << SQUARE(5) << endl;
+    cout << SQUARE(12) << endl;
+    return 0;
+}*/
+
+template <typename T>
+inline T SQUARE(T x)
+{
+    return x * x;
+}
+
+int main(void)
+{
+    cout << SQUARE(5.5) << endl;
+    cout << SQUARE(12) << endl;
+    return 0;
+}
